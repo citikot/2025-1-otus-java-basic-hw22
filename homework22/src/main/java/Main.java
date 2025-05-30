cfe664942d030ba4bc1427e3edba25d1f11b045d
citikot <citikot@dr.com>
2025-05-30 21:49:37 +0300

    created hw22 structure

---
+++ homework22/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import service.ArrayService;
+
+public class Main {
+
+    private static ArrayService arrayService = new ArrayService();
+
+    public static void main(String[] args) {
+
+    }
+}
